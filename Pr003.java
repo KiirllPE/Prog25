1d079afd1ace8e6e48a83f47c476a797913797ee
polyntsev <polyncevkirill924@gmail.com>
2025-09-02 13:09:44 +0700

    Обьявление переменных и преобразование типов данных

---
+++ Pr003.java
@@ -0,0 +1,34 @@
+class Pr003 {
+	public static void main(String agrs[]) {
+		//Обьявление целых переменых
+		int var1, var2 = 20;
+		//Обьявление логической перменой
+		boolean varb=true;
+		//Обьявление вещественных переменных
+		double vard1=10.3, vard2;
+		var1 =10;
+		//Проверка условия
+		if (var1>var2) {
+			int var3 = 300;
+			System.out.println(var3);
+		}
+		else {
+			int var4 = 300;
+			System.out.println(var4);
+
+		}
+		if (varb) {
+			System.out.println(var1);
+		}
+
+		//Деление двух целых чисел
+		System.out.println("Результат деления var1 на var2");
+		System.out.println(var1/var2);
+		
+		//Приведение результата деления к типу double
+		vard2 = (double)var1/var2;
+		System.out.println("Результат деления var1 на var2  c  преоброзованием в doumle"); 
+		System.out.println(vard2);
+	}	
+}
+
