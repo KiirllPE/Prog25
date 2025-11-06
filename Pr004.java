60888d33806014600e734a4ae4b48da84a7959c0
KiirllPE <gggjjjfff0006@gmail.com>
2025-11-06 12:13:36 +0700

    Демонстрация операций сравнения и логических операций

--- Pr004.java
+++ Pr004.java
@@ -1,32 +1,71 @@
 class Pr004 {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		//Арифметические операции
-		System.out.println("Знак операции \tВыполняемое действие");
+		System.out.println("Знак операции\tВыполняемое действие");
 		System.out.println("+\t\tСложение\n-\t\tВычитание\n*\t\tУмножение\n/\t\tДеление");
-		System.out.println("%\t\tДеление по модолю\n++\t\tИнкремент\n--\t\tДекремент");
+		System.out.println("%\t\tДеление по модулю\n++\t\tИнкримент\n--\t\tДекремент");
 
 		//Операции сравнения
-		System.out.println("\nЗнак операции \tВыполняемое действие");
-		System.out.println("==\t\tравенство\n!=\t\tнеравенство\n>\t\tБольше\n<\t\tменьше");
-		System.out.println(">=\t\tБольше или равно\n<=\t\tМеньше или равно");
+		
+		System.out.println("\nЗнак операции\tВыполняемое действие");
+                System.out.println("==\t\tРавенство\n!=\t\tНеравенство\n>\t\tБольше\n<\t\tМеньше");
+                System.out.println(">=\t\tБольше или равно\n<=\t\tМеньше или равно");
+
 		//Логические операции
-		System.out.println("\nЗнак операции \tВыполняемое действие");
-		System.out.println("%\t\tИ\n|\t\tИЛИ\n^\t\tИсключаещее ИЛИ");
-		System.out.println("&&\t\tУскоренное И\n||\t\tУскоренное ИЛИ\n!\t\tНЕ");
+		
+		System.out.println("\nЗнак операции\tВыполняемое действие");
+                System.out.println("&\t\tИ\n|\t\tИЛИ\n^\t\tИсключающее ИЛИ");
+		System.out.println("&&\t\tУкороченное И\n||\t\tУкороченное ИЛИ\n!\t\tНЕ");
 
 		//Примеры выполнения логических операций
-		System.out.println();
+		System.out.print("\n");
 		System.out.println("p\tq\tp&q\tp|q\tp^q\t!p");
-		boolean p=false, q=false;
-		System.out.println(p + "\t" + q + "\t"+ (p&q) + "\t" + (p|q) + "\t" + (p^q) + "\t" + !p);
+		boolean p = false, q = false;
+		System.out.println(p + "\t" + (q) + "\t" + (p&q) + "\t"+ (p|q) + "\t" + (p^q) + "\t" + (!p));
 		p = true;
-		System.out.println(p + "\t" + q + "\t"+ (p&q) + "\t" + (p|q) + "\t" + (p^q) + "\t" + !p);
+		System.out.println(p + "\t" + (q) + "\t" + (p&q) + "\t"+ (p|q) + "\t" + (p^q) + "\t" + (!p));
 		q = true; p = false;
-		System.out.println(p + "\t" + q + "\t"+ (p&q) + "\t" + (p|q) + "\t" + (p^q) + "\t" + !p);
-		p = false;
-		System.out.println(p + "\t" + q + "\t"+ (p&q) + "\t" + (p|q) + "\t" + (p^q) + "\t" + !p);
+		System.out.println(p + "\t" + (q) + "\t" + (p&q) + "\t"+ (p|q) + "\t" + (p^q) + "\t" + (!p));
+		p = true; 
+		System.out.println(p + "\t" + (q) + "\t" + (p&q) + "\t"+ (p|q) + "\t" + (p^q) + "\t" + (!p));
+
+		//Применение оператора if с операциями сравнения и логическими операциями
+		int a1, a2;
 		
-	
+		boolean b1, b2;
+		System.out.println("");
+		a1 = 10;
+		a2 = 11;
+		if(a1 < a2) System.out.println("a1 < a2");
+		if(a1 <= a2) System.out.println("a1 <= a2");
+		if(a1 != a2) System.out.println("a1 != a2");
+		if(a1 >= a2) System.out.println("Не выполняется");
+		if(a1 == a2) System.out.println("Не выполняется");
+		if(a1 > a2) System.out.println("Не выполняется");
+
+		b1 = true;
+		b2 = false;
+		if(b1 & b2) System.out.println("Не выполняется");
+		if(!(b1 & b2)) System.out.println("!(b1 & b2): true");
+		if(b1 | b2) System.out.println("b1 | b2: true");
+		if(b1 ^ b2) System.out.println("b1 ^ b2: true");
+
+		//Применение укороченных логических операций
+		
+		int n, d;
+		n = 10;
+		d = 2;
+		System.out.println("");
+		if(d != 0 && (n%d) == 0)
+			System.out.println(d + " является делителем " + n);
+
+		d = 0;
+		if(d != 0 && (n%d) == 0)
+                        System.out.println(d + " является делителем " + n);
+
+		//Обычный логический оператор
+		
+		if(d != 0 && (n%d) == 0)
+                        System.out.println(d + " является делителем " + n);
 	}
 }
-
